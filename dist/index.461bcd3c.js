4fca52dac4ed848d4d3841d5dae8710ad1965457
carles-escude <75261579+carles-escude@users.noreply.github.com>
2023-10-01 18:53:39 +0200

    Commit inicial

---
+++ dist/index.461bcd3c.js
@@ -0,0 +1,3 @@
+console.log("Hello world!");//# sourceMappingURL=index.461bcd3c.js.map
+
+//# sourceMappingURL=index.461bcd3c.js.map
---
+++ dist/index.b5bcf7ac.js
@@ -0,0 +1,3 @@
+console.log("Hello world!");//# sourceMappingURL=index.b5bcf7ac.js.map
+
+//# sourceMappingURL=index.b5bcf7ac.js.map
